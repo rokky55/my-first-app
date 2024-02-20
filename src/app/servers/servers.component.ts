c51d5a69ed06a90640f028b73d839c14776f5fa7
rokky55 <rokky55@yahoo.com>
2024-02-20 17:28:08 +0530

    committing the pending source folders

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //selector: '[app-servers]', //In attribute selector style way
+  //selector: '.app-servers', ////In class selector style way
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
